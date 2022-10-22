a13ef3d5c5eb57799533da10da621c52255e9571
usorama <usorama@gmail.com>
2022-10-22 10:25:34 +0530

    First Commit - Provider and VPC

---
+++ Terraform/AddlCourses/Terraform-freecodecamp-Utube/main.tf
@@ -0,0 +1,11 @@
+# AWS VPC
+
+resource "aws_vpc" "uu-vpc" {
+    cidr_block = "10.123.0.0/16"
+    enable_dns_hostnames = true
+    enable_dns_support = true      
+
+    tags = {
+      "Name" = "uu-dev"
+    }
+}
